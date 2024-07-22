994ea44607f234bf58af308472807d7dc0bc43a3
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2024-07-22 18:48:35 -0500

    Config Module
    Config module for handling configuration across the project.

---
+++ app/modules/config.py
@@ -0,0 +1,34 @@
+import yaml
+import os
+import munch
+import pathlib
+
+
+# import log
+# logger = log.log(__name__,color=True)
+
+
+def load_config(config_file: str | pathlib.Path):
+    if type(config_file) == pathlib.Path:
+        # if a pathlib is passed in, convert path to str
+        config_file = str(config_file)
+
+    # logger.info(f"Loading config '{config_file}'")
+    # yaml loader
+    with open(config_file, "r") as f:
+        config = munch.munchify(yaml.safe_load(f))
+
+    # non-config file values (env vars, etc)
+    config.example = "somevalue"
+
+    # return munch object
+    return config
+
+
+if __name__ == "__main__":
+    # ONLY works from in here, as root path in not in app dir
+    c = load_config(config_file=pathlib.Path.cwd() / "app" / "config" / "config.yaml")
+
+    print(c.example)
+    # yaml key
+    print(c.server.name)
