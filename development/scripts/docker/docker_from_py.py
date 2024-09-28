689de119c0e90485e59396678b4ef6ebc86eb35b
ryanq.47 <ryankleffman47@gmail.com>
2024-09-27 21:54:39 -0400

    docker tests

---
+++ development/scripts/docker/docker_from_py.py
@@ -0,0 +1,19 @@
+import docker
+
+# Initialize the Docker client
+client = docker.from_env()
+
+# Pull an image
+client.images.pull('alpine')
+
+# Run a container
+container = client.containers.run('alpine', 'echo hello world', detach=True)
+print(container.logs())
+
+# List running containers
+for container in client.containers.list():
+    print(container.name)
+
+# Stop and remove the container
+container.stop()
+container.remove()
