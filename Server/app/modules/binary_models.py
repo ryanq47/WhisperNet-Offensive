fec37f4f7277302785b5d0ba50401171fd2fd953
ryanq.47 <ryankleffman47@gmail.com>
2024-10-09 16:04:40 -0400

    Update binary_models.py

--- Server/app/modules/binary_models.py
+++ Server/app/modules/binary_models.py
@@ -5,6 +5,8 @@ from modules.docker_builder import DockerBuilder
 from modules.log import log
 logger = log(__name__)
 
+# Next - fix binary_buuilder to talk to this, and test. 
+
 class Agent():
     def __init__(self, build_target, server_address, server_port, executable_name):
         self.build_target = build_target 
@@ -16,13 +18,15 @@ class Agent():
         self.output_dir = str((Config().launch_path / "data" / "compiled").absolute())
         self.build_context   = str(Config().root_project_path)
 
-        # get values based on target/config - unsure best way to do this
+        # get values based on target/config
         #self.build_options = Config().config.server.binaries.agents.x86_windows_agent
+        self.build_options = Config().config.server.binaries.agents.get(build_target)
 
     def build(self):
-        ...
         try:
             logger.info(f"Building {self.build_target}")
+            # debug print items
+            logger.debug(options for options in self.build_options.items())
             # make an async function or threaded so it can just be called.
 
             # build the binary
@@ -33,8 +37,11 @@ class Agent():
             )
 
             docker_instance.execute()
+            return True
+        
         except Exception as e:
             logger.error(f"Could not build binary: {e}")
+            raise e
 
     
 
